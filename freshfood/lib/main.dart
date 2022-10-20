b61488d7de961e4e34392966e3adee83736fa8c3
KIMERA ERIASAF <eriasaf31@gmail.com>
2022-10-20 16:28:38 +0300

    changed

--- freshfood/lib/main.dart
+++ freshfood/lib/main.dart
@@ -1,24 +1,113 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-import 'package:freshfood/farmerspage.dart';
-
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Fresh products',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        primarySwatch: Colors.blue,
-      ),
-      home: const Farmer(),
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
     );
+  }
+}
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final Marker _kGooglePlexMarker = Marker(
+      markerId: MarkerId('_kGooglePlex'),
+      infoWindow: InfoWindow(title: 'Google Plex'),
+      icon: BitmapDescriptor.defaultMarker,
+      position: LatLng(37.42796133580664, -122.085749655962));
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  static final Marker _kLakeMarker = Marker(
+      markerId: MarkerId('_kLakeMarker'),
+      infoWindow: InfoWindow(title: 'Lake'),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+      position: LatLng(37.42796133580664, -122.085749655962));
+  static final Polyline _kPolyline = Polyline(
+    polylineId: PolylineId('_kPolyline'),
+    points: [
+      LatLng(37.42796133580664, -122.085749655962),
+      LatLng(37.43296265331129, -122.08832357078792),
+    ],
+    width: 5,
+  );
+
+  static final Polygon _kPolygon = Polygon(
+    polygonId: PolygonId('_kPolygon'),
+    points: [
+      LatLng(37.42796133580664, -122.085749655962),
+      LatLng(37.43296265331129, -122.08832357078792),
+      LatLng(37.418, -122.092),
+      LatLng(37.418, -122.092),
+    ],
+    strokeWidth: 5,
+    fillColor: Colors.transparent,
+  );
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(title: Text('google maps')),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(child: TextFormField()),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.search),
+              ),
+            ],
+          ),
+          Expanded(
+            child: GoogleMap(
+              mapType: MapType.normal,
+              markers: {
+                _kGooglePlexMarker,
+                _kLakeMarker,
+              },
+              polylines: {
+                _kPolyline,
+              },
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
