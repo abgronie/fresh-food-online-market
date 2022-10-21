22193071f92663e9f7978b242636a96c1542d9b3
KIMERA ERIASAF <eriasaf31@gmail.com>
2022-10-21 10:22:51 +0300

    complete

--- freshfood/lib/main.dart
+++ freshfood/lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:freshfood/location_service.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(MyApp());
@@ -22,49 +24,67 @@ class MapSample extends StatefulWidget {
 
 class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
+  TextEditingController _originController = TextEditingController();
+  TextEditingController _destinationController = TextEditingController();
+
+  Set<Marker> _markers = Set<Marker>();
+  Set<Polygon> _polygons = Set<Polygon>();
+  Set<Polyline> _polylines = Set<Polyline>();
+  List<LatLng> polygonLatLngs = <LatLng>[];
+
+  int _polygonIdCounter = 1;
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
 
-  static final Marker _kGooglePlexMarker = Marker(
-      markerId: MarkerId('_kGooglePlex'),
-      infoWindow: InfoWindow(title: 'Google Plex'),
-      icon: BitmapDescriptor.defaultMarker,
-      position: LatLng(37.42796133580664, -122.085749655962));
+  @override
+  void initState() {
+    super.initState();
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+    _setMarker(LatLng(37.42796133580664, -122.085749655962));
+  }
 
-  static final Marker _kLakeMarker = Marker(
-      markerId: MarkerId('_kLakeMarker'),
-      infoWindow: InfoWindow(title: 'Lake'),
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-      position: LatLng(37.42796133580664, -122.085749655962));
-  static final Polyline _kPolyline = Polyline(
-    polylineId: PolylineId('_kPolyline'),
-    points: [
-      LatLng(37.42796133580664, -122.085749655962),
-      LatLng(37.43296265331129, -122.08832357078792),
-    ],
-    width: 5,
-  );
+  void _setMarker(LatLng point) {
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId('marker'),
+          position: point,
+        ),
+      );
+    });
+  }
 
-  static final Polygon _kPolygon = Polygon(
-    polygonId: PolygonId('_kPolygon'),
-    points: [
-      LatLng(37.42796133580664, -122.085749655962),
-      LatLng(37.43296265331129, -122.08832357078792),
-      LatLng(37.418, -122.092),
-      LatLng(37.418, -122.092),
-    ],
-    strokeWidth: 5,
-    fillColor: Colors.transparent,
-  );
+  void _setPolygon() {
+    final String polygonIdVal = 'polygon_$_polygonIdCounter';
+    _polygonIdCounter++;
+
+    _polygons.add(Polygon(
+        polygonId: PolygonId(polygonIdVal),
+        points: polygonLatLngs,
+        strokeWidth: 2,
+        fillColor: Colors.transparent));
+  }
+
+  void _setPolyline(List<PointLatLng> points) {
+    var _polylineIdCounter;
+    final String polylineIdVal = 'polyline_$_polylineIdCounter';
+    _polylineIdCounter++;
+
+    _polylines.add(Polyline(
+      polylineId: PolylineId(polylineIdVal),
+      width: 2,
+      color: Colors.blue,
+      points: points
+          .map(
+            (point) => LatLng(point.latitude, point.longitude),
+          )
+          .toList(),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -73,41 +93,112 @@ class MapSampleState extends State<MapSample> {
         children: [
           Row(
             children: [
-              Expanded(child: TextFormField()),
+              Expanded(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _originController,
+                      decoration: InputDecoration(hintText: 'search by market'),
+                      onChanged: (value) {
+                        print(value);
+                      },
+                    ),
+                    TextFormField(
+                      controller: _destinationController,
+                      decoration: InputDecoration(hintText: 'search by market'),
+                      onChanged: (value) {
+                        print(value);
+                      },
+                    )
+                  ],
+                ),
+              ),
               IconButton(
-                onPressed: () {},
+                onPressed: () async {
+                  var directions = await LocationService().getDirections(
+                      _originController.text, _destinationController.text);
+                  _goToPlace(
+                    directions['start_location']['lat'],
+                    directions['start_location']['lng'],
+                    directions['bound_ne'],
+                    directions['bound_sw'],
+                  );
+                  _setPolyline(directions['polyline_decoded']);
+                },
                 icon: Icon(Icons.search),
               ),
             ],
           ),
+
+          // Row(
+          //   children: [
+          //     Expanded(
+          //       child: TextFormField(
+          //         controller: _searchController,
+          //         textCapitalization: TextCapitalization.words,
+          //         decoration: InputDecoration(hintText: 'search by market'),
+          //         onChanged: (value) {
+          //           print(value);
+          //         },
+          //       ),
+          //     ),
+          //     IconButton(
+          //       onPressed: () async {
+          //         var place =
+          //             await LocationService().getPlace(_searchController.text);
+          //         _goToPlace(place);
+          //       },
+          //       icon: Icon(Icons.search),
+          //     ),
+          //   ],
+          // ),
           Expanded(
             child: GoogleMap(
               mapType: MapType.normal,
-              markers: {
-                _kGooglePlexMarker,
-                _kLakeMarker,
-              },
-              polylines: {
-                _kPolyline,
-              },
+              markers: _markers,
+              polygons: _polygons,
+              polylines: _polylines,
               initialCameraPosition: _kGooglePlex,
               onMapCreated: (GoogleMapController controller) {
                 _controller.complete(controller);
+              },
+              onTap: (point) {
+                setState(() {
+                  polygonLatLngs.add(point);
+                  _setPolygon();
+                });
               },
             ),
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
+      //floatingActionButton: FloatingActionButton.extended(
+      //onPressed: _goToTheLake,
+      //label: Text('To the lake!'),
+      //icon: Icon(Icons.directions_boat),
     );
   }
 
-  Future<void> _goToTheLake() async {
+  Future<void> _goToPlace(
+    //Map<String, dynamic> place
+    double lat,
+    double lng,
+    Map<String, dynamic> boundsNe,
+    Map<String, dynamic> boundsSw,
+  ) async {
+    //final double lat = place['geometry']['location']['lat'];
+    //final double lng = place['geometry']['location']['lng'];
+
     final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    controller.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(target: LatLng(lat, lng), zoom: 12),
+    ));
+    controller.animateCamera(CameraUpdate.newLatLngBounds(
+        LatLngBounds(
+            southwest: LatLng(boundsSw['lat'], boundsSw['lng']),
+            northeast: LatLng(boundsNe['lat'], boundsNe['lng'])),
+        25));
+
+    _setMarker(LatLng(lat, lng));
   }
 }
